428609023d49dfe986dabd24c74316746806b7b3
xectray1 <unknownerror9338@gmail.com>
2025-10-06 11:10:53 +0200

    Create wsd.lua

---
+++ wsd.lua
@@ -0,0 +1,41 @@
+if not (game:IsLoaded()) then game.Loaded:Wait(); end;
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/Library.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/addons/SaveManager.lua"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/addons/ThemeManager.lua"))()
+local Window = Library:CreateWindow({
+    Title = "                         scripthookv       weird strict dad(lobby)",
+    Center = true,
+    AutoShow = true,
+})
+
+local Tabs = {
+    Main = Window:AddTab("main"),
+    ['settings'] = Window:AddTab('settings'),
+}
+
+local MainBox = Tabs.Main:AddLeftGroupbox("main")
+MainBox:AddButton("teleport to chapter 1", function()
+    cloneref(game:GetService("TeleportService")):Teleport(14787381917, game.Players.LocalPlayer);
+end);
+MainBox:AddButton("teleport to chapter 2", function()
+    cloneref(game:GetService("TeleportService")):Teleport(15322497988, game.Players.LocalPlayer);
+end);
+MainBox:AddButton("teleport to chapter 3", function()
+    cloneref(game:GetService("TeleportService")):Teleport(16375066410, game.Players.LocalPlayer);
+end);
+MainBox:AddButton("teleport to chapter 4", function()
+    cloneref(game:GetService("TeleportService")):Teleport(17619037026, game.Players.LocalPlayer);
+end);
+MainBox:AddButton("teleport to book 1 chapter 1", function()
+    cloneref(game:GetService("TeleportService")):Teleport(14787369036, game.Players.LocalPlayer);
+end);
+local MenuGroup = Tabs['settings']:AddLeftGroupbox('ui')
+MenuGroup:AddLabel('toggle ui'):AddKeyPicker("uitoggle", { Default = 'End', NoUI = true, Text = 'UI Bind' })
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+Library.ToggleKeybind = Options.uitoggle
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({ 'uitoggle' })
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:BuildConfigSection(Tabs['settings'])
+ThemeManager:ApplyToTab(Tabs['settings'])
