29031dd5e07aba0593d9bd6edd88aefc25760848
xectray1 <unknownerror9338@gmail.com>
2025-10-06 16:59:46 +0200

    Update wsd.lua

--- wsd.lua
+++ wsd.lua
@@ -29,6 +29,7 @@ end);
 MainBox:AddButton("teleport to chapter 3", function()
     queue_on_teleport[[
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xectray1/realloader/refs/heads/main/books.lua"))()
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/Library.lua"))()
     Library:Notify("chapter 3 not released sory", 5);
     ]]
     cloneref(game:GetService("TeleportService")):Teleport(16375066410, game.Players.LocalPlayer);
@@ -36,6 +37,7 @@ end);
 MainBox:AddButton("teleport to chapter 4", function()
     queue_on_teleport[[
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xectray1/realloader/refs/heads/main/books.lua"))()
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/refs/heads/main/Library.lua"))()
     Library:Notify("chapter 4 not released sory", 5);
     ]]
     cloneref(game:GetService("TeleportService")):Teleport(17619037026, game.Players.LocalPlayer);
